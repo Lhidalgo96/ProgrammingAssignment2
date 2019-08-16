530abebdf34737cefb4aacd4b77857895b969495
Lhidalgo96 <52679796+Lhidalgo96@users.noreply.github.com>
2019-08-15 22:27:30 -0600

    Code Solved
    Create and test both functions required for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
+# Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates and sets the values of the matrix and then saves its inverse to an object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inv <- NULL
+        set <- function(y)
+        {
+                x <<- y ##To assign value from different environement
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv  ##inverse the Matrix
+        
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) ##Cache the mean
 }
 
 
-## Write a short comment describing this function
+## Checks the inverse value in the cache, and then gets the result and skips computation or sets the value into the cache
+## depending on the first check, uses the setInverse to assign the value of the matrix, and then retrieve it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+        inv <- x$getInverse()
+        if (!is.null(inv)) #If its already calculated, and the matrix hasn't change, returns the value
+        {
+                return(inv) 
+        }
+        ##If not will compute
+        mat <- x$get()  #Computes the inverse of the matrix (If squared)
+        inv <- solve(mat, ...) 
+        x$setInverse(inv)
+        inv
 }
